07779d7ab2dc260dfe002509df0ae7216e1efe86
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-04-30 19:42:41 +0100

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by revangelista on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
