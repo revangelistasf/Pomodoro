13668fca9832bbfd59d5615ad826ade8d9636528
Roberto Evangelista <r.evangelista@swordhealth.com>
2024-12-05 22:04:28 +0000

    start using spm to organize the project

---
+++ Pomodoro/Models/Task.swift
@@ -0,0 +1,33 @@
+//
+//  Task.swift
+//  Pomodoro
+//
+//  Created by Roberto Evangelista on 23/11/2024.
+//
+
+import Foundation
+
+struct Todo {
+	var type: TaskType
+	var title: String
+
+	var time: Date? {
+		switch type {
+		case .todo:
+			return self.estimatedCompletion
+		case .done:
+			return self.finishedAt
+		}
+	}
+	// Only used when task is todo
+	var estimatedCompletion: Date?
+	var repeatedTimes: Int = 1
+
+	// Only used when task is done
+	var finishedAt: Date? = nil
+	var taskLength: TimeInterval? = nil
+
+	enum TaskType {
+		case todo, done
+	}
+}
